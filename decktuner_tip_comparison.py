5b3359afe9f40b6e9b40069b4b62227ab80b656e
WitchPHD <74690105+WitchPHD@users.noreply.github.com>
2023-07-18 21:40:18 -0400

    Update decktuner_tip_comparison.py

--- decktuner_tip_comparison.py
+++ decktuner_tip_comparison.py
@@ -11,9 +11,15 @@ class TIP:
         #Get the Ko-Fi stripe TIP
         self.KF_STRIPE_FEE = (dollars * 0.029) + 0.30
         self.KF_STRIPE_TIP = dollars - self.KF_STRIPE_FEE
-        #Get the Paypal stripe TIP
-        self.KF_PAYPAL_FEE = (dollars * 0.030) + 0.45
+        #Get the Ko-Fi stripe INTERNATIONAL TIP
+        self.KF_STRIPEIN_FEE = (dollars * 0.029) + (dollars * 0.015) + 0.30
+        self.KF_STRIPEIN_TIP = dollars - self.KF_STRIPEIN_FEE
+        #Get the Ko-Fi PayPal TIP
+        self.KF_PAYPAL_FEE = (dollars * 0.0289) + 0.49
         self.KF_PAYPAL_TIP = dollars - self.KF_PAYPAL_FEE
+        #Get the Ko-Fi PayPal INTERNATIONAL TIP
+        self.KF_PAYPALIN_FEE = (dollars * 0.0289) + (dollars * 0.015) + 0.49
+        self.KF_PAYPALIN_TIP = dollars - self.KF_PAYPALIN_FEE
 
 print ('Please enter all tips in $USD$ dollar amounts without special characters.\n')
 print ('When you are finished entering tips, type \'done\' and hit enter.\n')
@@ -29,6 +35,10 @@ while True:
         break
 
 #generate some counters
+KF_STRIPEIN_FEE_TOT = 0
+KF_STRIPEIN_TIP_TOT = 0
+KF_PAYPALIN_FEE_TOT = 0
+KF_PAYPALIN_TIP_TOT = 0
 KF_STRIPE_FEE_TOT = 0
 KF_STRIPE_TIP_TOT = 0
 KF_PAYPAL_FEE_TOT = 0
@@ -40,6 +50,10 @@ print ('\n')
 for t in tips:
     #print each tip and add them to counters
     print("{:.2f}$ tip costs: {:.2f}$ BMAC, {:.2f}$ Ko-Fi (Stripe), and {:.2f}$ Ko-Fi (Paypal) ".format(t.amount, t.BMAC_FEE, t.KF_STRIPE_FEE, t.KF_PAYPAL_FEE))
+    KF_STRIPEIN_FEE_TOT = KF_STRIPEIN_FEE_TOT + t.KF_STRIPEIN_FEE
+    KF_STRIPEIN_TIP_TOT = KF_STRIPEIN_TIP_TOT + t.KF_STRIPEIN_TIP
+    KF_PAYPALIN_FEE_TOT = KF_PAYPALIN_FEE_TOT + t.KF_PAYPALIN_FEE
+    KF_PAYPALIN_TIP_TOT = KF_PAYPALIN_TIP_TOT + t.KF_PAYPALIN_TIP
     KF_STRIPE_FEE_TOT = KF_STRIPE_FEE_TOT + t.KF_STRIPE_FEE
     KF_STRIPE_TIP_TOT = KF_STRIPE_TIP_TOT + t.KF_STRIPE_TIP
     KF_PAYPAL_FEE_TOT = KF_PAYPAL_FEE_TOT + t.KF_PAYPAL_FEE
@@ -48,8 +62,14 @@ for t in tips:
     BMAC_TIP_TOT = BMAC_TIP_TOT + t.BMAC_TIP
 
 print ('\nRESULTS FOR {:} TIPS:\n'.format(n-1))
-print ('BuyMeACoffee: you pay {:.2f}$ in fees and pocket {:.2f}$.'.format(BMAC_FEE_TOT, BMAC_TIP_TOT))
-print ('Ko-Fi (Stripe): you pay {:.2f}$ in fees and pocket {:.2f}$.'.format(KF_STRIPE_FEE_TOT, KF_STRIPE_TIP_TOT))
-print ('Ko-Fi (Paypal): you pay {:.2f}$ in fees and pocket {:.2f}$.'.format(KF_PAYPAL_FEE_TOT, KF_PAYPAL_TIP_TOT))
-print ('Ko-Fi (Average): you pay {:.2f}$ in fees and pocket {:.2f}$.'.format((KF_PAYPAL_FEE_TOT+KF_STRIPE_FEE_TOT)/2, (KF_PAYPAL_TIP_TOT+KF_STRIPE_TIP_TOT)/2))
-input('Hit ENTER to terminate the program.')
+print ('BuyMeACoffee:')
+print ('    BuyMeACoffee: you pay {:.2f}$ in fees and pocket {:.2f}$.'.format(BMAC_FEE_TOT, BMAC_TIP_TOT))
+print ('    (w/ "cover credit card fees" unchecked') 
+print ('\nKo-Fi:')
+print ('    Ko-Fi (Stripe): you pay {:.2f}$ in fees and pocket {:.2f}$.'.format(KF_STRIPE_FEE_TOT, KF_STRIPE_TIP_TOT))
+print ('    Ko-Fi (Paypal): you pay {:.2f}$ in fees and pocket {:.2f}$.'.format(KF_PAYPAL_FEE_TOT, KF_PAYPAL_TIP_TOT))
+print ('    Ko-Fi (Stripe - International): you pay {:.2f}$ in fees and pocket {:.2f}$.'.format(KF_STRIPEIN_FEE_TOT, KF_STRIPEIN_TIP_TOT))
+print ('    Ko-Fi (Paypal - International): you pay {:.2f}$ in fees and pocket {:.2f}$.'.format(KF_PAYPALIN_FEE_TOT, KF_PAYPALIN_TIP_TOT))
+print ('    Ko-Fi (Average): you pay {:.2f}$ in fees and pocket {:.2f}$.'.format((KF_PAYPAL_FEE_TOT+KF_STRIPE_FEE_TOT+KF_PAYPALIN_FEE_TOT+KF_STRIPEIN_FEE_TOT)/4, (KF_PAYPAL_TIP_TOT+KF_STRIPE_TIP_TOT+KF_PAYPALIN_TIP_TOT+KF_STRIPEIN_TIP_TOT)/4))
+input('\nHit ENTER to terminate the program.')
+
